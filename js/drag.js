be578b76140a2c1b7d689cd53223bc0874e94dca
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-04-04 22:49:44 -0400

    begin multi select (ms)

--- js/drag.js
+++ js/drag.js
@@ -1,12 +1,13 @@
 //https://stackoverflow.com/questions/24050738/javascript-how-to-dynamically-move-div-by-clicking-and-dragging
 //Why does this use ID and not class? Performance. But why doesn't querySelector work here?
 
+//Bug-fix - try changing div from 'draggable' to 'canvas'
 function drag_div(div){
   var mousePosition;
   var offset = [0,0];
   var isDown = false;
 
-  div = document.getElementById(div); //querySelector doesn't seem to work here. Performance is better by ID (but will it work on multi-select??)
+  div = document.getElementById(div); //querySelector doesn't seem to work here. TryPerformance is better by ID (but will it work on multi-select??)
 
   // if(event.target.matches('dragging')) { //buggy (end bracket below)
   // if(div.classList.contains('dragging')) { //buggy (end bracket below)
@@ -36,6 +37,3 @@ function drag_div(div){
   }, true);
   // } //buggy
 }
-
-// drag_div('draggable'); // call this in resize.js using the toggleDown functino (selection)
-// drag_div('canvas');
--- js/selection.js
+++ js/selection.js
@@ -1,14 +1,15 @@
 // SELECTION
-//Major bug -- draggable and resize conflict. Try using a class called 'enabled'? Or set a global variable 'resizing' and only allow dragging when it's false. Try taking out the ID.
+//BUG (SEVERE) -- draggable and resize conflict. Try using a class called 'enabled'? Or set a global variable 'resizing' and only allow dragging when it's false. Try taking out the ID or using something to replace it.
 //BUG -- can drag element when dragging canvas
 //BUG -- dragging fails on first try
+
+//Enhance -- grey background box on selection and drag, like Balsamiq?
 
 //https://codepen.io/bxchang04/pen/abOreoP
 // Toggle resizing border and resizers on/off. Called on a parent element for performance reasons.
 function toggleDown() {
-  if (!event.target.closest('.dragging')){ //bug -- it can drag even if not clicking target
-  drag_div('draggable');
-  }
+  drag_div('draggable'); //bugfix - try using 'canvas' instead and modify drag.js to use firstElementChild
+  this.firstElementChild.classList.add('dragging'); //currently not in use
 }
 
 function toggleUp() {
@@ -25,8 +26,9 @@ function toggleUp() {
   if (!event.target.closest('.resizable')) {
     this.firstElementChild.classList.remove('resizable');
   }
-}
 
+  this.firstElementChild.classList.remove('dragging'); //currently not in use
+}
 // Add event listeners for toggling on and off
 document.querySelector('.canvas').addEventListener('mousedown', toggleDown); //change canvas to ID? And canvas can have only 1 child.
 document.querySelector('.canvas').addEventListener('mouseup', toggleUp); //change canvas to ID? And canvas can have only 1 child.
