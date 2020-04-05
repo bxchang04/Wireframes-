79941d4db1f56e6109d93956f00ecb84e78969bd
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-04-04 22:23:46 -0400

    select AKA toggle implemented

--- js/selection.js
+++ js/selection.js
@@ -2,34 +2,29 @@
 //Major bug -- draggable and resize conflict. Try using a class called 'enabled'? Or set a global variable 'resizing' and only allow dragging when it's false. Try taking out the ID.
 //BUG -- can drag element when dragging canvas
 //BUG -- dragging fails on first try
-//BUG -- mouseUp after drag select firstChild
 
 //https://codepen.io/bxchang04/pen/abOreoP
 // Toggle resizing border and resizers on/off. Called on a parent element for performance reasons.
 function toggleDown() {
   if (!event.target.closest('.dragging')){ //bug -- it can drag even if not clicking target
-  // if (!this.firstElementChild.classList.contains('.dragging')){
-  this.firstElementChild.classList.toggle('dragging');
   drag_div('draggable');
   }
 }
 
 function toggleUp() {
-  console.log("toggle")
-  console.log("classes = " + event.target.classList)
-
-  // if (!event.target.matches('.dragging')) { //doesn't work -- should prevent selection after drag
-    makeResizableDiv('.resizable') //moved into toggleUp so that it only runs when a resizeable element is clicked up. But BUG -- only works on second click.
-  // }
-  this.firstElementChild.classList.toggle('dragging');
-
   // If closest element to click is an already selected element, bail
   if (event.target.closest('.resizable')) return
 
-  // Otherwise if element clicked is not selected, select
-  // if (event.target.classList.contains('.resizable__off')) {
-    this.firstElementChild.classList.toggle('resizable');
-  // }
+  // Otherwise if element clicked is not already selected, select
+  if (!event.target.closest('.resizable')) {
+    this.firstElementChild.classList.add('resizable');
+    makeResizableDiv('.resizable') //moved into toggleUp so that it only runs when a resizeable element is clicked up. But BUG -- only works on second click.
+  }
+
+  // If element clicked is not the currently selected object, unselect
+  if (!event.target.closest('.resizable')) {
+    this.firstElementChild.classList.remove('resizable');
+  }
 }
 
 // Add event listeners for toggling on and off
