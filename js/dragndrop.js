9b3f47ad1b89f1a2dfc872d9976409aadb39bb7c
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-04-07 15:57:07 -0400

    share w/ larry

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -3,9 +3,9 @@ var dnd = dragNdrop({
   element: document.getElementById('draggable1'),
   // dropElements and/or CSS Selector(s) (false / DOM element(s))
 });
-/*
+
 dragNdrop({
   // element to be dragged (DOM element)
   element: document.getElementById('draggable2'),
   // dropElements and/or CSS Selector(s)
-});*/
+});
